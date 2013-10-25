e1d69d1a553c9995cdf2f905aacf343f54b5c419
Thor Kell <thor.kell@mail.mcgill.ca>
2013-10-25 17:53:12 -0400

    Adding redis, trying to actually push

--- hello.py
+++ hello.py
@@ -3,6 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
+def get_artist_data(artist_name):
+    pass
+
+
+def find_artist_data(artist_name):
+    pass
+
+
 @app.route('/', methods=['GET'])
 def hello():
     return "feminest server 1.0"
