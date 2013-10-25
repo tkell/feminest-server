66c5a8e07f601c0ac9b3db4e30dfe6d77d4782d3
Thor Kell <thor.kell@mail.mcgill.ca>
2013-10-25 18:51:41 -0400

    Trying to get redis going

--- hello.py
+++ hello.py
@@ -1,13 +1,19 @@
 import os
 from flask import Flask
+from flask import request
+
+import redis
+
+#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+#redis = redis.from_url(redis_url)
+
+#url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+#r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 
 app = Flask(__name__)
 
-
-
 def get_artist_data(artist_name):
     pass
-
 
 def find_artist_data(artist_name):
     pass
@@ -17,11 +23,19 @@ def find_artist_data(artist_name):
 def hello():
     return "feminest server 1.0"
 
-@app.route('/get-data', methods=['GET'])
-def get_data():
-    return "feminest::get"
+@app.route('/get-data/<artist_name>', methods=['GET'])
+def get_data(artist_name):
+
+    # and get the same hard-coded key here
+    #redis.set('test-key', 'test-value')
+
+    return "feminest::get %s" % artist_name
 
 
-@app.route('/find-data', methods=['GET'])
-def find_data():
-    return "feminest::find"
+@app.route('/find-data/<artist_name>', methods=['GET'])
+def find_data(artist_name):
+
+    # need to write something to redis here
+    #val = redis.get('test-key', 'test-value')
+
+    return "feminest::find %s - %s" % (artist_name, val)
