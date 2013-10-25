b8bc2de6d0f6c48af2f62598e4b911149abe3dfc
Thor Kell <thor.kell@mail.mcgill.ca>
2013-10-25 19:23:11 -0400

    Commenting

--- hello.py
+++ hello.py
@@ -5,17 +5,18 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+import pyechonest
+
 import redis
 url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost'))
 redis = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 app = Flask(__name__)
 
+
 def get_artist_data(artist_name):
     pass
 
-def find_artist_data(artist_name):
-    pass
 
 
 @app.route('/', methods=['GET'])
@@ -24,23 +25,21 @@ def hello():
 
 @app.route('/get-data/<artist_name>', methods=['GET'])
 def get_data(artist_name):
-
-    print "in get data, with %s" % artist_name
-
-    # OK, so this will read the JSON back out.  Rad.  
+    '''Get the logged data for a given artist name''' 
     res_string = redis.get(artist_name)
-    print "read %s from redis" % res_string
-
     res_dict = json.loads(res_string)
-    print "made dict:  %s" % res_dict
-
     return jsonify(**res_dict)
 
 
 @app.route('/find-data/<artist_name>', methods=['GET'])
 def find_data(artist_name):
+    '''Find data and write data for a given artist name'''
+
+    # do the work.
+    get_artist_data(artist_name)
+
 
     # need to write something to redis here
+    # CAREFUL ABOUT JSON FORMATTTTINGGG
     redis.set(artist_name, '{"artist": "%s"}' % artist_name)
-
     return "feminest::find %s" % (artist_name)
